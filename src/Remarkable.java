ad54fccbadd3eb68a778f8dfc4625ebbe00e1d70
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-31 13:24:52 -0700

    SecretMessageBox,TheRiddler

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -5,7 +5,16 @@ public static void main(String[] args) {
 	String answer = JOptionPane.showInputDialog("What's your name");
 	if (answer.equals("Kassandra")) {
 		JOptionPane.showMessageDialog(null, "Cool necklace");
+		String shirt = JOptionPane.showInputDialog("What's your name");
+		if (answer.equals("Roberto")) {
+			JOptionPane.showMessageDialog(null, "Cool shirt");
+			String jacket = JOptionPane.showInputDialog("What's your name");
+			if (answer.equals("Jacob")) {
+				JOptionPane.showMessageDialog(null, "Cool jacket");
 	}
+		
 	
+}
+	}
 }
 }
