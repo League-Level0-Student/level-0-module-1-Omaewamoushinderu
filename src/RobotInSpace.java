62d80fdbb2eda565be2450362bba44b17060d0ea
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-07 13:21:46 -0700

    TheRiddler,RobotInSpace

--- src/RobotInSpace.java
+++ src/RobotInSpace.java
@@ -24,16 +24,19 @@ import org.jointheleague.graphical.robot.Robot;
 
 	private void moveRobot(int keyPressed) throws InterruptedException {
 	    // 2. Print out the keyPressed variable and write down the numbers for each arrow key
-
+System.out.println(keyPressed);  //up 38 down 40 left 39 right is 37                                                            
 	    // 3. If the up arrow is pressed, move the Robot up the screen.
-		
+if(keyPressed == 38) {
+	rob.move(10);
+}
 	    // 4. If the down arrow is pressed, move the Robot down.
-
+  
 	    // 5. If the left arrow is pressed, make the Robot go left.
 	   
 	    // 6. If right is pressed, move the Robot right.
 	    
 	    // 7. Run your program and move the Robot to RD-2D for a surprise! 
+          //run my program or give me all your money
 	}
 
 	private void checkIfR2D2Found() throws Exception {
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -14,14 +14,42 @@ int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 String riddle = JOptionPane.showInputDialog("I have hands I can move but I cannot clap, what am I?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-if (riddle.equals("a clock"));
+if (riddle.equals("clock")){
 JOptionPane.showMessageDialog(null, "correcto mundo");
-		// 5. Otherwise, say "wrong" and tell them the answer
-
+score++;
+JOptionPane.showMessageDialog(null, "congrats you scored" + score);
+}		// 5. Otherwise, say "wrong" and tell them the answer
+else {
+	JOptionPane.showMessageDialog(null, "WROOOOOOOONG!!!");
+	
+}
 		// 6. Add some more riddles
-
+String riddler = JOptionPane.showInputDialog("They try to beat me, they try in vain.\n" + 
+		"And when I win, I end the pain.");
+if(riddler.equals("death")) {
+JOptionPane.showMessageDialog(null, "correcto mundo");
+score++;
+	JOptionPane.showMessageDialog(null, "congrats you scored" + score);
+}
+else {
+	JOptionPane.showMessageDialog(null, "WROOOOOOOONG!!!");
+	
+}
 		// 2. Make a pop up to show the score.
 
-	}
+String riddlee = JOptionPane.showInputDialog(null, "What is so delicate that saying its name breaks it?");
+if(riddlee.equals("silence")) {
+	JOptionPane.showMessageDialog(null, "correcto mundo");
+	score++;
+		JOptionPane.showMessageDialog(null, "congrats you scored" + score);
 }
+else {
+	JOptionPane.showMessageDialog(null, "WROOOOOOOONG!!!");
+	
+
+}
+}
+}
+
+
 
